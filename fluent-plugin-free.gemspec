835156301e6f629231da0cc4b4a1193d4b40a67f
zonomasa <tatezono@gmail.com>
2014-06-28 11:15:35 +0900

    firsrt commit

---
+++ fluent-plugin-free.gemspec
@@ -0,0 +1,24 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'fluent/plugin/free/version'
+
+Gem::Specification.new do |spec|
+  spec.name          = "fluent-plugin-free"
+  spec.version       = 0.0.1
+  spec.authors       = ["TATEZONO Masaki"]
+  spec.email         = ["tatezono@gmail.com"]
+  spec.summary       = %q{Input plugin for fluentd to collect memory usage from free command.}
+  spec.description   = %q{Input plugin for fluentd to collect memory usage from free command.}
+  spec.homepage      = "https://github.com/zonomasa/fluent-plugin-free"
+  spec.license       = "APLv2"
+
+  spec.files         = `git ls-files -z`.split("\x0")
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.6"
+  spec.add_development_dependency "rake"
+  spec.add_runtime_dependency "fluentd"
+end
---
+++ lib/fluent/plugin/in_free.rb
@@ -0,0 +1,63 @@
+class FreeInput < Fluent::Input
+  Fluent::Plugin.register_input('free', self)
+
+  config_param :interval, :time,   :default => nil
+  config_param :option,   :string, :default => nil
+  config_param :unit,     :string, :default => 'mega'
+  config_param :mode,     :string, :default => nil
+  config_param :tag,      :string, :default => 'memory.free'
+
+  def configure(conf)
+    super
+    if @interval
+      @tick = interval.to_i
+    else
+      @tick = 10
+    end
+
+    @option = case @unit
+              when 'byte' then '-b'
+              when 'kilo' then '-k'
+              when 'mega' then '-m'
+              when 'giga' then '-g'
+              else
+                raise RuntimeError, "@unit must be one of byte/kilo/mega/giga"
+              end
+  end
+
+  def start
+    super
+    @thread = Thread.new(&method(:run))
+  end
+
+  def shutdown
+    @thread.terminate
+    @thread.join
+  end
+
+  def run
+    loop do
+      Fluent::Engine.emit(@tag, Fluent::Engine.now, get_free_info)
+      sleep @tick
+    end
+  end
+
+  private
+  def get_free_info
+    ret = `free #{@option}`.split($/)
+    ret.shift
+    if @mode == 'actual'
+      items = ret[1].split(/\s+/)
+    else
+      items = ret[0].split(/\s+/)
+    end
+    free_info = {
+      'used'    => items[2],
+      'free'    => items[3],
+    }
+    free_info
+  end
+
+
+
+end
